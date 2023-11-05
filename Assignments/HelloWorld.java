c261d3a38a72bfdcdf2a2ddae5a9b68c4d41fab3
Sumanth007 <sumanthhegde356@gmail.com>
2023-11-05 13:52:19 +0530

    HelloWold.java modified

--- Assignments/HelloWorld.java
+++ Assignments/HelloWorld.java
@@ -3,5 +3,11 @@ package classWork.Assignments;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int nums[] = {1, 2, 3, 4};
+
+        for(int n :nums)
+        {
+            System.out.println(n);
+        }
     }
 }
