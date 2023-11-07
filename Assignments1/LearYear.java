5b7b475864aad919f05aba373fb366f4373ba272
Sumanth <61139248+Sumanth007@users.noreply.github.com>
2023-11-07 18:17:32 +0530

    Update LearYear.java

--- Assignments1/LearYear.java
+++ Assignments1/LearYear.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class LearYear {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        // Scanner in = new Scanner(System.in);
 
 
         while(true) {
