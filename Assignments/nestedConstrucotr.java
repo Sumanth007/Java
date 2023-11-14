4402e355e1aeb829ebd1c8ba3a9433e27bde34e3
Raze777 <samrexz7884@gmail.com>
2023-11-14 23:48:30 +0530

    changes in nested contructor

--- Assignments/nestedConstrucotr.java
+++ Assignments/nestedConstrucotr.java
@@ -3,17 +3,20 @@ package classWork.Assignments;
 public class nestedConstrucotr {
     int primaryCon;
     int secondaryCon;
-    nestedConstrucotr(){
+
+    nestedConstrucotr() {
         this(1);
         this.primaryCon = 10;
     }
 
-    nestedConstrucotr(int x){
+    nestedConstrucotr(int x) {
         this.secondaryCon = 100;
     }
 
     public static void main(String[] args) {
         nestedConstrucotr obj = new nestedConstrucotr();
         System.out.println(obj.primaryCon + " " + obj.secondaryCon);
+
+//        Runtime.getRuntime().gc();
     }
 }
