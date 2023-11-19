4e617462ce3c564ac5d8e1d60f9b244975292afb
Raze777 <samrexz7884@gmail.com>
2023-11-19 20:51:47 +0530

    Guess the name added

--- Assignments/postiveFirstNegatvieSecondZeroLast.java
+++ Assignments/postiveFirstNegatvieSecondZeroLast.java
@@ -1,10 +1,10 @@
-package classWork.Assignments;
-import java.util.Arrays;
-public class postiveFirstNegatvieSecondZeroLast {
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(display(new int[]{1,2,-4,0,0,5,6})));
-    }
-    public static int[] display(int[] arr) {
-
-    }
-}
+//package classWork.Assignments;
+//import java.util.Arrays;
+//public class postiveFirstNegatvieSecondZeroLast {
+//    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(display(new int[]{1,2,-4,0,0,5,6})));
+//    }
+//    public static int[] display(int[] arr) {
+//
+//    }
+//}
